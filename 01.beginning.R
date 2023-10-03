1a845b2840efc11b4a294d389d9872bf96ed1252
angiperuz <146854729+angiperuz@users.noreply.github.com>
2023-10-03 17:04:50 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,18 @@
+#here I can write anything I want. 
+
+#R is a calculator
+2+3
+
+#Assign to an object
+zima <- 2+3
+zima
+
+
+duccio <- 5+3
+duccio
+
+zima*duccio
+final <- zima*duccio
+final
+
+final^2
